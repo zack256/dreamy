a7d495391fd57209a4079378b5a53a35be5f23ef
zack256 <20821071+zack256@users.noreply.github.com>
2020-10-28 15:51:25 -0400

    simple page up

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page_view():
     return "hello :)"
+
+@app.route("/write-on-image/")
+def write_on_image_page():
+    return render_template("write_on_image.html")
+
+@app.route("/assets/<path:file_path>")
+def get_asset_file(file_path):
+    path = os.path.join(os.path.dirname(__file__), "assets")
+    return send_from_directory(path, file_path, as_attachment = True)
